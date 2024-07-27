8395fbd5b5ca10e5903a86a14413164be6a43147
PaulSPuntusov <pps_ru@mail.ru>
2024-07-27 18:47:25 +0300

    Class Dot

---
+++ src/Dot.java
@@ -0,0 +1,17 @@
+public class Dot {
+    int x, y;
+
+    public Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
